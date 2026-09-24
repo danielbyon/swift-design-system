1b29a9b9b842dc225e09b9817cc4d736f075d678
Daniel Byon <daniel@danielbyon.com>
2026-09-24 05:51:40 -0700

    Bootstrap cross-platform Swift package

---
+++ Examples/Shared/ExampleApp.swift
@@ -0,0 +1,11 @@
+import DesignSystem
+import SwiftUI
+
+@main
+struct DesignSystemExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("DesignSystem example host")
+        }
+    }
+}
---
+++ Sources/DesignSystemTestSupport/DesignSystemTestSupport.swift
@@ -0,0 +1,2 @@
+// The test-support module depends on the production module but exposes no helpers yet.
+import DesignSystem
